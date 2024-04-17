84de2455464255a4f25139cd2b0588824b8d9317
riapis <fari.hafizh@ui.ac.id>
2024-04-17 14:06:29 +0700

    initial commit

---
+++ src/test/java/id/ac/ui/cs/advprog/profile/ProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
