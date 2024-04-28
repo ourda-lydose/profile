212b752508646d9f005e0565c00dca92be3673cb
riapis <fari.hafizh@ui.ac.id>
2024-04-29 01:11:09 +0700

    [RED] add test for Profile service

--- src/main/java/id/ac/ui/cs/advprog/profile/repository/ProfileRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/profile/repository/ProfileRepository.java
@@ -1,10 +1,12 @@
 package id.ac.ui.cs.advprog.profile.repository;
 
 import id.ac.ui.cs.advprog.profile.model.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class ProfileRepository implements ProfileRepositoryInterface{
     private List<User> userList = new ArrayList<>();
 
