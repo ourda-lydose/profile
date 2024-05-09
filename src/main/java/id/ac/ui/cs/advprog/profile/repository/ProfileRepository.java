7892e7be56be5af42c6dcbb07d3265e5102a2e93
riapis <fari.hafizh@ui.ac.id>
2024-05-09 13:32:12 +0700

    [Red] Implement Profile repository interface

--- src/main/java/id/ac/ui/cs/advprog/profile/repository/ProfileRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/profile/repository/ProfileRepository.java
@@ -1,55 +1,12 @@
 package id.ac.ui.cs.advprog.profile.repository;
 
 import id.ac.ui.cs.advprog.profile.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-public class ProfileRepository implements ProfileRepositoryInterface{
-    private List<User> userList = new ArrayList<>();
-
-    public User create(User user) {
-        User existingUser = findById(user.getId());
-
-        if (existingUser != null) {
-            int index = userList.indexOf(existingUser);
-            userList.set(index, user);
-        } else {
-            userList.add(user);
-        }
-
-        return user;
-    }
-
-    public User delete(User User){
-        userList.remove(User);
-        return User;
-    }
-
-    public User findById(int id){
-        for(User savedUser : userList){
-            if(savedUser.getId().equals(id)){
-                return savedUser;
-            }
-        }
-        return null;
-    }
-
-
-    public User edit(User updatedUser){
-        for (int i = 0; i < userList.size(); i++) {
-            User User = userList.get(i);
-            if(User.getId().equals(updatedUser.getId())){
-                userList.set(i, updatedUser);
-                return updatedUser;
-            }
-        }
-        return null;
-    }
-
-    public List<User> findAll(){
-        return userList;
-    }
+public interface ProfileRepository extends JpaRepository<User, String> {
 }
