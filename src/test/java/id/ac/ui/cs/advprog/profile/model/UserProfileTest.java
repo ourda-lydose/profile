e7f71493f1161ccf254c32af173d47bb7099035f
riapis <fari.hafizh@ui.ac.id>
2024-05-20 13:27:46 +0700

    fix: add lack import

--- src/test/java/id/ac/ui/cs/advprog/profile/model/UserProfileTest.java
+++ src/test/java/id/ac/ui/cs/advprog/profile/model/UserProfileTest.java
@@ -3,6 +3,8 @@ package id.ac.ui.cs.advprog.profile.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserProfileTest {
